7520a5a5646142db90983ad8cfb69e8f4b9e094e
minhasorquideas <nickolasbarreiros@gmail.com>
2026-07-23 18:17:03 -0300

    Create orquideas.js

---
+++ dados/orquideas.js
@@ -0,0 +1,37 @@
+const orquideas = [
+    {
+        nome: "Dendrobium polysema",
+        genero: "Dendrobium",
+        tipo: "Espécie botânica",
+        origem: "Nova Guiné",
+        clima: "Quente e úmido",
+        iluminacao: "Luz filtrada intensa",
+        floracao: "Final do inverno e primavera",
+        substrato: "Casca de pinus, carvão, pedra-pomes e pouco esfagno",
+        dificuldade: "Moderada",
+
+        fotos: [
+            "imagens/dendrobium-polysema-1.jpg",
+            "imagens/dendrobium-polysema-2.jpg",
+            "imagens/dendrobium-polysema-3.jpg"
+        ]
+    },
+
+    {
+        nome: "Bifrenaria verboonenii",
+        genero: "Bifrenaria",
+        tipo: "Espécie botânica",
+        origem: "Mata Atlântica brasileira",
+        clima: "Quente a ameno e úmido",
+        iluminacao: "Luz filtrada intensa",
+        floracao: "Primavera e verão",
+        substrato: "Casca de pinus, carvão, pedra-pomes e pouco esfagno",
+        dificuldade: "Moderada",
+
+        fotos: [
+            "imagens/bifrenaria-verboonenii-1.jpg",
+            "imagens/bifrenaria-verboonenii-2.jpg",
+            "imagens/bifrenaria-verboonenii-3.jpg"
+        ]
+    }
+];
