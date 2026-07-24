dbc25f2f1a18e46562375e213a02ca19b5ed3496
minhasorquideas <nickolasbarreiros@gmail.com>
2026-07-23 22:12:35 -0300

    Add files via upload

---
+++ dados/orquideas/sophronitis-cernua.js
@@ -0,0 +1,33 @@
+// Cadastro gerado a partir do álbum de orquídeas.
+// Preencha os campos vazios quando revisar esta planta.
+
+export const sophronitisCernua = {
+    id: "sophronitis-cernua",
+    nome: "Sophronitis cernua",
+    genero: "Sophronitis",
+    tipo: "",
+    dificuldade: "",
+    caracteristicas: [],
+    fotos: [],
+    descricao: "",
+    origem: "",
+    regiao: "",
+    habitat: "",
+    clima: "",
+    iluminacao: "",
+    floracao: "",
+    adubacao: "",
+    rega: "",
+    suporte: "",
+    substrato: "",
+    dica: "",
+    mesesFloracao: [],
+    avaliacoes: {
+        cultivo: 0,
+        floracao: 0,
+        perfume: 0,
+        luminosidade: 0,
+        agua: 0,
+        raridade: 0
+    }
+};
