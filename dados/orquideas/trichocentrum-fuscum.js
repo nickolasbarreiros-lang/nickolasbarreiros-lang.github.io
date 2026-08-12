044f8129c2d4704a07470d3f0986f8002d0f97f5
minhasorquideas <nickolasbarreiros@gmail.com>
2026-08-11 22:41:29 -0300

    aa
    aa

--- dados/orquideas/trichocentrum-fuscum.js
+++ dados/orquideas/trichocentrum-fuscum.js
@@ -24,7 +24,7 @@ export const trichocentrumFuscum = {
         "imagens/Trichocentrum fuscum/Trichocentrum fuscum3.jpg",
         "imagens/Trichocentrum fuscum/Trichocentrum fuscum4.webp"
     ],
-    "descricao": "Compacta e de aparência quase suculenta, Trichocentrum fuscum se destaca pelas folhas grossas e carnosas, que armazenam água e permitem à planta atravessar períodos de secagem entre as chuvas. É uma epífita da América do Sul tropical e produz flores em tons de amarelo, ocre e castanho, geralmente com labelo mais claro e contrastante. Seu formato já revela a melhor estratégia de cultivo: muita claridade filtrada, ar em movimento e raízes que recebam uma boa rega, mas sequem rapidamente antes do próximo molhamento.",
+    "descricao": "Compacta e de aparência quase suculenta, Trichocentrum fuscum se destaca pelas folhas grossas e carnosas, que armazenam água e permitem à planta atravessar períodos de secagem entre as chuvas. É uma epífita da América do Sul tropical e produz flores em tons de amarelo, ocre e castanho, geralmente com labelo mais claro e contrastante. Seu formato já revela a melhor estratégia de cultivo: muita claridade filtrada, ar em movimento e raízes que recebam uma boa rega, mas sequem rapidamente antes da próxima rega.",
     "origem": "América do Sul tropical.",
     "regiao": "América do Sul tropical.",
     "habitat": "Floresta tropical úmida; epífita.",
