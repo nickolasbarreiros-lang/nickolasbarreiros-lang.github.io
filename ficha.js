91b6fe75916ee226bbb8e2db2bb00db18e7bc3a6
minhasorquideas <nickolasbarreiros@gmail.com>
2026-09-22 12:57:32 -0300

    gg
    gg

--- ficha.js
+++ ficha.js
@@ -504,6 +504,7 @@ function criarFormasCultivoV4(config, perfilRadicular = []) {
                         : nome.includes("árvore") || nome.includes("arvore") ? "🌳"
                         : nome.includes("cesto") ? `<img class="icone-img-cultivo-v421" src="imagens/ui/cesto-madeira-orquideas.webp" alt="">`
                         : nome.includes("barro") ? `<img class="icone-img-cultivo-v421" src="imagens/ui/vaso-barro-orquideas-v438.webp" alt="">`
+                : (nome === "Vaso plástico" || nome === "Vaso plástico comum") ? `<img class="icone-img-cultivo-v421" src="imagens/ui/vaso-plastico-comum-v449.webp" alt="">`
                         : nome.includes("vaso") ? "🪴"
                         : nome.includes("placa") || nome.includes("tronco") ? "🪵" : "🌿";
                     return `
